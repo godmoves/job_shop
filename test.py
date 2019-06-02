6ea2dd620823021f782d799779421fce3a18a10d
ManKit Pong <mankitpong@gmail.com>
2019-06-03 01:54:28 +0800

    update result, add distributions

--- test.py
+++ test.py
@@ -3,17 +3,17 @@ from timer import Timer
 from ants_algo import AntsAlgorithm
 
 
-ID = 3
+ID = 4
 
 # Task 1 & 2
 aa = AntsAlgorithm(case_id=ID, verbose=False, random_mode=False)
 aa_order = aa.run(epoch_num=500)
-aa_timer = Timer(case_id=ID, orders=aa_order, random_mode=False)
-aa_timer.get_total_time(repeat=1)
+# aa_timer = Timer(case_id=ID, orders=aa_order, random_mode=True)
+# aa_timer.get_total_time(repeat=100000)
 
 # # Task 2
 # optimal_timer = Timer(case_id=ID, orders=ORDERS[ID], random_mode=True)
-# optimal_timer.get_total_time(repeat=1000)
+# optimal_timer.get_total_time(repeat=100000)
 
 # # Task 1
 # optimal_timer = Timer(case_id=ID, orders=ORDERS[ID], random_mode=False)
